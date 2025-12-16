9b5047fb7be4063a0a98f802aee925fba99621a1
Abdullah105 <abdallah1052001@gmail.com>
2025-12-16 14:45:17 +0200

    Adding CRUD operation On design controller

---
+++ Backend/Shatabli.Core.Application/Features/Designs/Commands/SoftDeleteDesign/DesignSoftDeleteCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shatabli.Core.Application.Interfaces;
+
+namespace Shatabli.Core.Application.Features.Designs.Commands.SoftDeleteDesign
+{
+    public class DesignSoftDeleteCommandHandler : IRequestHandler<DesignSoftDeleteCommand, DesignSoftDeleteResponse>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DesignSoftDeleteCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        async Task<DesignSoftDeleteResponse> IRequestHandler<DesignSoftDeleteCommand, DesignSoftDeleteResponse>.Handle(DesignSoftDeleteCommand request, CancellationToken cancellationToken)
+        { 
+           var designFromDb = _context.Designs
+                .Where(d => d.Id == request.designId).AsTracking().FirstOrDefault();
+            //designFromDb.IsDeleted = true;
+
+            _context.Designs.Remove(designFromDb);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            DesignSoftDeleteResponse response = new();
+            response.Success = true;
+            return response;
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend/Shatabli.Core.Application/Features/Designs/Queries/GetAllDesigns/GetAllDesignsMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Shatabli.Core.Domain.Entities;
+
+namespace Shatabli.Core.Application.Features.Designs.Queries.GetAllDesigns
+{
+    public class GetAllDesignsMapper : Profile
+    {
+        public GetAllDesignsMapper() 
+        {
+            CreateProjection<Design, GetAllDesignDTO>();
+        }    
+    }
+}
